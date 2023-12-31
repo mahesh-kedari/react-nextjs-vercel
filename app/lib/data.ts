f592c250cebf8772e1cf4e352babc3da8ea9d5e6
Mahesh Kedari <mahesh@mulyam.in>
2023-12-31 09:50:52 +0530

    Form handling added

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -156,6 +156,7 @@ export async function fetchInvoicesPages(query: string) {
 
 export async function fetchInvoiceById(id: string) {
   noStore();
+  console.log('ID is ', id);
   try {
     const data = await sql<InvoiceForm>`
       SELECT
@@ -172,7 +173,7 @@ export async function fetchInvoiceById(id: string) {
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
-
+    console.log(invoice); // Invoice is an empty array []
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
